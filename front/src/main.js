7c4b2f415bf7c79a455fb264ae66f98edc6657aa
Reterer <egor-suhanov2012@yandex.ru>
2023-05-13 10:20:33 +0300

    add simple graph

--- front/src/main.js
+++ front/src/main.js
@@ -3,7 +3,15 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
 import axios from 'axios'
+
+import VNetworkGraph from "v-network-graph"
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+
 
 const app = createApp(App)
 
@@ -11,5 +19,7 @@ axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'http://localhost:5000/';  // the FastAPI backend
 
 app.use(router)
+app.use(VNetworkGraph)
+app.use(ElementPlus)
 
 app.mount('#app')
